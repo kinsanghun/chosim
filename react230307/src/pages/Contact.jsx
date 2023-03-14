ae1b4aa8c0e6eda3dda4dee5e0de2176f8146d9c
sanghunKim <schmid_t@naver.com>
2023-03-14 16:03:50 +0900

    Change post output method

--- react230307/src/pages/Contact.jsx
+++ react230307/src/pages/Contact.jsx
@@ -1,9 +1,40 @@
 import LineTitle from "components/LineTitle";
 
+
+function ContactItem({title, content}) {
+    return (
+        <div className="item">
+            <h3>{title}</h3>
+            <div>{content}</div>
+        </div>
+    )
+}
 export default function Contact() {
+    const contactList = [
+        {
+            title : "phone",
+            content : "010-5736-6491"
+        },{
+            title : "github",
+            content : "https://github.com/kinsanghun"
+        },{
+            title : "tistory",
+            content : "https://develophun.tistory.com/"
+        },{
+            title : "email",
+            content : "schmid_t@kakao.com"
+        }
+    ]
+
+    const items = contactList.map(data => <ContactItem 
+        key={data.title} 
+        title={data.title}
+        content={data.content}
+    />)
     return (
         <div className="contact">
             <LineTitle title={"contact"}/>
+            <div className="items">{items}</div>
         </div>
     )
 }
--- react230307/src/pages/Main.jsx
+++ react230307/src/pages/Main.jsx
@@ -2,76 +2,81 @@ import Carousel from "components/Carousel";
 import LineLayout from "components/LineLayout";
 import Post from "components/Post";
 
+const postList = [
+    {
+        url : require("assets/images/tmp12.jpg"),
+        title :  "Lorem Ipsum",
+        description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },{
+        url : require("assets/images/tmp11.jpg"),
+        title :  "Lorem Ipsum",
+        description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },{
+        url : require("assets/images/tmp10.jpg"),
+        title :  "Lorem Ipsum",
+        description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },{
+        url : require("assets/images/tmp9.jpg"),
+        title :  "Lorem Ipsum",
+        description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },{
+        url : require("assets/images/tmp8.jpg"),
+        title :  "Lorem Ipsum",
+        description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },{
+        url : require("assets/images/tmp7.jpg"),
+        title :  "Lorem Ipsum",
+        description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },{
+        url : require("assets/images/tmp6.jpg"),
+        title :  "Lorem Ipsum",
+        description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },{
+        url : require("assets/images/tmp5.jpg"),
+        title :  "Lorem Ipsum",
+        description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },{
+        url : require("assets/images/tmp4.jpg"),
+        title :  "Lorem Ipsum",
+        description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },{
+        url : require("assets/images/tmp3.jpg"),
+        title :  "Lorem Ipsum",
+        description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },{
+        url : require("assets/images/tmp2.jpg"),
+        title :  "Lorem Ipsum",
+        description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },{
+        url : require("assets/images/tmp1.jpg"),
+        title :  "Lorem Ipsum",
+        description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+    },
+]
+
 export default function Main() {
-    const postLeft = [
-        <Post
+    const postLeft = [];
+    const postRight = [];
 
-            key="8"
-            url={require("assets/images/tmp12.jpg")}
-            title={"Lorem Ipsum"}
-            description={"Lorem Ipsum is simply dummy text of the printing and typesetting industry."}
-        />,
-        <Post
+    postList.forEach((data, index) => {
+        const post = <Post key={`post_${index}`} title={data.title} description={data.description} url={data.url}/>
 
-            key="7"
-            url={require("assets/images/tmp11.jpg")}
-            title={"Lorem Ipsum"}
-            description={"Lorem Ipsum is simply dummy text of the printing and typesetting industry."}
-        />,
-        <Post
-            key="6"
-            url={require("assets/images/tmp5.jpg")}
-            title={"Lorem Ipsum"}
-            description={"Lorem Ipsum is simply dummy text of the printing and typesetting industry."}
-        />,
-        <Post
+        if(index % 2 == 0) {
+            postLeft.push(post);
+        }
+        else {
+            postRight.push(post);
+        }
+    })
 
-            key="5"
-            url={require("assets/images/tmp7.jpg")}
-            title={"Lorem Ipsum"}
-            description={"Lorem Ipsum is simply dummy text of the printing and typesetting industry."}
-        />
-    ];
-    const postRight = [
-        <Post
-
-            key="4"
-            url={require("assets/images/tmp2.jpg")}
-            title={"Lorem Ipsum"}
-            description={"Lorem Ipsum is simply dummy text of the printing and typesetting industry."}
-        />,
-        <Post
-
-            key="3"
-            url={require("assets/images/tmp4.jpg")}
-            title={"Lorem Ipsum"}
-            description={"Lorem Ipsum is simply dummy text of the printing and typesetting industry."}
-        />,
-        <Post
-            key="2"
-            url={require("assets/images/tmp6.jpg")}
-            title={"Lorem Ipsum"}
-            description={"Lorem Ipsum is simply dummy text of the printing and typesetting industry."}
-        />,
-        <Post
-            key="1"
-            url={require("assets/images/tmp9.jpg")}
-            title={"Lorem Ipsum"}
-            description={"Lorem Ipsum is simply dummy text of the printing and typesetting industry."}
-        />
-    ]
     return (
         <div className="main">
             <Carousel/>
             <div className="container">
                 <h2>GALLERYS</h2>
                 <div>
-                    <LineLayout>
-                        {postLeft}
-                    </LineLayout>
-                    <LineLayout>
-                        {postRight}
-                    </LineLayout>
+                    <LineLayout>{postLeft}</LineLayout>
+                    <LineLayout>{postRight}</LineLayout>
                 </div>
             </div>
         </div>
